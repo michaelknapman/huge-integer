f9ae5a8b17fa2bfe38897bcf3c4af93c826ec70b
michaelknapman <mknapman3221@gmail.com>
2021-01-14 23:35:16 -0500

    development

--- src/huge/HugeIntTiming.java
+++ src/huge/HugeIntTiming.java
@@ -2,9 +2,54 @@ package huge;
 
 public class HugeIntTiming {
 
+	
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//use java.math.BigInteger class and HugeInteger class
 
+		
+		//for fixed n this program should instantiate random integers of size n
+		//(n decimal digits)
+		//then run each of the operations on the integers
+		//measure the time required to perform each operation.
+		
+		//measuring the time requires System.currentTimeMillis()
+		//run the operations many times on a pair of integers.
+		//run at least 100 pairs of integers.
+		
+		//example code
+		
+		/*
+		HugeInteger huge1, huge2, huge3;
+		long startTime, endTime;
+		double runTime = 0.0;
+		for(int numInts = 0; numInts < MAXNUMINTS; numInts++){
+			huge1 = new HugeInteger(n); //creates random integer of n digits
+			huge2 = new HugeInteger(n); //creates random integer of n digits
+			startTime = System.currentTimeMillis();
+			for(int numRun=0; numRun < MAXRUN; numRun++){
+				huge3 = huge1.add(huge2);
+			}
+			endTime = System.currentTimeMillis();
+			runTime +=(double) (endTime - startTime)/((double) MAXRUN);
+		}
+		runTime = runTime/((double)MAXNUMINTS);
+		...
+		*/
+		
+		//for each of the operations, comparison
+		//subtraction, addition, multiplication
+		//measure the runnnig time for various values of n
+		//eg n = 10, 100, 500, 1000, 5000, 10000
+		
+		
+		
 	}
 
 }
--- src/huge/HugeInteger.java
+++ src/huge/HugeInteger.java
@@ -5,18 +5,52 @@ package huge;
 
 public class HugeInteger {
 
-	//private int placeholder; //placeholder
+	private byte[] NumArr; //array holding the hugeinteger
+	private String[] StringArr; //intermediate characters.
+	private boolean negative; //true if negative. false if positive.
+	private int digits; //how many digits long
+	
+
+	//in order to implement hugemungus, we can probably just do a
+	//phat array of integers 0-9.
+	//the array should only be as long as the thing that you put in it
+	//eg 2543 should make a 4 long array
+	//eg "42" should make a 2 long array
+	
+	//but how do we handle negatives? 
+	//probably should just have a flag if negative.
+	//if - * + then - etc
+	//also if the -ve has more digits than the +ve, output is -ve
+	//etc
+	
+	//how to add?
+	//loop from the right digit and move left
+	//take note of any carry's 
+	//keep on moving left
 	
 	
 	//constructors
-	
 	public HugeInteger(String val) {
 		
 		//used to create a hugeinteger from a decimal string
 		//needs to account for minus sign.
 		//must throw an exception if other characters used.
 		
-		//this.placeholder = stuff
+		//what is length?
+		this.digits = val.length(); //need to -1 for '\0'? idk
+		
+		//is it negative?
+		//parse string for a -ve sign
+		//this.negative = 0 if positive, = 1 if negative.
+		this.negative = val.contains("-");
+		
+		//split string into chars
+		//"" is a regex meaning split every character. 
+		this.StringArr = val.split("");
+		
+		//now convert everything in StringArr to NumArr
+		
+		
 		
 	}
 	
